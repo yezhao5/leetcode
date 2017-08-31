b963c8d82c403a2342cf0c1714e082d646f6e809
yezhao5 <yezhao@ucsd.edu>
2017-08-31 02:41:30 -0700

    7 add correct sol

--- 7_ReversedInteger/sol.cpp
+++ 7_ReversedInteger/sol.cpp
@@ -1,10 +1,10 @@
-ass Solution {
+class Solution {
     
 public:
     int reverse(int x) {
         vector<int> reversed;
         int neg = 1;
-        int low = -2147483648;
+        int low = 2147483648;
         unsigned int up = 2147483647;
         
         if(x<0){
@@ -28,14 +28,18 @@ public:
         unsigned int sum = 0;
         for(int j=0; j<reversed.size();j++){
             unsigned int a = count*reversed[j];
+            //cout<<"a is "<<a<<endl;
             if(a/count!=reversed[j]){
+                //cout<<"not okay"<<endl;
                 return 0;
             }
             sum+=a;
             
             count = count/10;
         }
-        
+        //cout<<"sum is"<<sum<<endl;
+        if(neg==1&&sum>up) return 0;
+        if(neg==-1&&sum>low) return 0;
         sum = sum*neg;
        
         
