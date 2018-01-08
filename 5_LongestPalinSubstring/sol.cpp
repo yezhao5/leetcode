41fd2d11b4b3a5e22902430d15bd10e442f6a537
yezhao5 <yezhao@ucsd.edu>
2018-01-08 11:31:51 -0800

    53

--- 5_LongestPalinSubstring/sol.cpp
+++ 5_LongestPalinSubstring/sol.cpp
@@ -4,39 +4,28 @@ public:
         if (s.size() ==0 ||s.size() == 1) return s;
         //assume each char can be the middle of the palindrom
         //if two adjcent chars are the same, assume it to be the middle
-        int max = 1, end = 0, start = 0;
+        int max = 1, start = 0;
         for (int i = 0; i < s.size()-1; i++){
-            int im, ie, is;
-            //case single middle
-            if(s[i+1] != s[i]){
-                im=1; is=i; ie=i;
-                int a=1;
-                while(i-a > -1 && i+a < s.size()){
-                    if(s[i-a] == s[i+a]){
-                        is = i-a;
-                        ie = i+a;
-                        im = im + 2;
-                    }
-                    a++;
-                }
+            int im, is;
+            //skip duplicate
+            int d = 0;
+            while(i+d < s.size() &&  s[i+d+1] == s[i]){
+                d++;
             }
-            //assume double middle
-            if(s[i+1] == s[i]){
-                im = 2; is = i, ie = i+1;
-                int a = 1;
-                while(i-a > -1 && i+a+1 < s.size()){
-                    if(s[i-a] == s[i+a+1]){
-                        is = i-a;
-                        ie = i+a+1;
-                        im = im + 2;
-                    }
-                    a++;
-                }
+
+            im = d+1; is = i;
+            int b = 1;
+
+            while(i-b > -1 && i+b+d < s.size() && s[i-b] == s[i+b+d]){
+                //cout<<"here"<<endl;
+                is = i-b;
+                im = 2*b + d + 1;
+
+                b++;
             }
             if(im> max) {
                 max = im;
                 start = is;
-                end = ie;
             }
         }
         return s.substr(start, max);
